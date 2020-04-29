57ce0ddf72ee2cfeed7c395016f53e281efa0061
juxnmxG <61563571+juxnmxG@users.noreply.github.com>
2020-04-29 10:26:13 -0500

    Update and rename Control_auditoria en mongoDB to Control_auditoria_en_mongoDB.js

---
+++ Control_auditoria_en_mongoDB.js
@@ -0,0 +1,31 @@
+// comando con el cual mongoDB tiene la auditoria sobre una base de datos//
+
+
+const { MongoClient } = require('mongodb');
+
+async function main() {
+    /**
+     * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+     * See http://bit.ly/NodeDocs_lauren for more details
+     */
+    const uri = "mongodb+srv://<username>:<password>@<your-cluster-url>/test?retryWrites=true&w=majority";
+    
+    /**
+     * The Mongo Client you will use to interact with your database
+     * See bit.ly/Node_MongoClient for more details
+     */
+    const client = new MongoClient(uri);
+
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();
+
+        // Make the appropriate DB calls
+
+    } finally {
+        // Close the connection to the MongoDB cluster
+        await client.close();
+    }
+}
+
+main().catch(console.error);
