407e0c6adf05f81bda5815b77274495cb43dcb3c
juxnmxG <61563571+juxnmxG@users.noreply.github.com>
2020-04-29 14:09:21 -0500

    Update Control_auditoria_en_mongoDB.js

--- Control_auditoria_en_mongoDB.js
+++ Control_auditoria_en_mongoDB.js
@@ -9,25 +9,22 @@ async function main() {
      * 
      */
     const uri = "mongodb+srv://<username>:<password>@<your-cluster-url>/test?retryWrites=true&w=majority";
-    
+    //metodo de monitoreo// activacion
     await monitorListingsUsingStreamAPI(client);
-    /**
-     * 
-     * 
-     */
+
     const client = new MongoClient(uri);
 
     try {
         //conexion  al cluster de mongoDB
         await client.connect();
-        
-       const result0 = await client.db("estudiante").collection("").updateOne({  });
+       //insercion de datos
+       const result0 = await client.db("escuela").collection("estudiante").updateOne({ "est_nom" : "Juan M" });
        console.log(`${result.matchedCount} document(s) matched the query criteria.`);
         
-       const result1 = await client.db("estudiante").collection("").insertOne();
+       const result1 = await client.db("escuela").collection("facultad").insertOne({ "fac_nom" : "ingenierias" });
        console.log(`New listing created with the following id: ${result.insertedId}`);
-       
-       const result2 = await client.db("estudiante").collection("").deleteOne({  });
+       //eliminacion de datos 
+       const result2 = await client.db("escuela").collection("materia").deleteOne({ "mat_id" : "00"  });
         
     } finally {
         // Cierre de la conexion con el cluster 
@@ -38,7 +35,7 @@ async function main() {
     
   
 }
-
+// funcion de monitoreo de la base de datos
 async function monitorListingsUsingStreamAPI(client, timeInMs = 60000, pipeline = []) { 
     
     const collection = client.db("escuela").collection("estudiante");
